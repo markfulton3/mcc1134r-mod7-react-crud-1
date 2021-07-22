709a42d0a3faa7032a2439ab86cc13f899e7c950
Mark Fulton <markfulton3@gmail.com>
2021-07-22 10:27:17 -0500

    init

---
+++ 1-InClass/blog-crud/src/App.js
@@ -0,0 +1,49 @@
+import './App.css';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import About from './pages/About';
+import Articles from './pages/Articles';
+import {Route, Switch} from 'react-router-dom';
+import Drawer from './components/Drawer';
+import settings from './data/settings';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    display: "flex"
+  },
+  appBar: {
+    width: `calc(100% - ${settings.drawerWidth}px)`,
+    height: theme.spacing(10),
+    marginLeft: '{drawerWidth}px',
+  },
+  
+  content: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing(5),
+    marginTop: theme.spacing(10),
+    height: '90vh'
+  },
+}));
+
+const App = () => {
+
+  const classes = useStyles();
+
+  return (
+    <div className={classes.container}>
+      <Drawer />
+      <main className={classes.content}>
+        <Switch>
+          <Route exact from="/" render={props => <Home {...props} />} />
+          <Route exact from="/contact" render={props => <Contact {...props} />} />
+          <Route exact from="/about" render={props => <About {...props} />} />    
+          <Route exact from="/articles" render={props => <Articles {...props} />} />          
+        </Switch>
+      </main>
+    </div>
+  );
+}
+
+export default App;
